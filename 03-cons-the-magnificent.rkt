d4abb8863318939759d56cdf90b519e991f2b1ff
Prathyush <hi@prabros.com>
2015-09-04 22:58:59 +0100

    Half way through third chapter

--- 03-cons-the-magnificent.rkt
+++ 03-cons-the-magnificent.rkt
@@ -46,3 +46,95 @@
 (rember a lat)
 ;; => '(lettuce and tomato)
 
+(define a 'and)
+
+(define lat '(bacon lettuce and tomato))
+
+(rember a lat)
+
+(define rember
+  (lambda (a lat)
+    (cond
+     ((null? lat) '())
+     (else (cond
+            ((eq? (first lat) a) (rest lat))
+            (else (cons (first lat) (rember a (rest lat)))))))))
+
+(define a 'and)
+
+(define lat '(bacon lettuce and tomato))
+
+(rember a lat)
+;; => '(bacon lettuce tomato)
+
+;; I am getting a feeling of unwinding and winding up the stack
+;; as I move through this finger exercises.
+
+;; Simplify only when you have corrected the function.
+;; Make the function's structure same as the argument's structure.
+;; Then simplify.
+
+(define a 'sauce)
+
+(define lat '(soy sauce and tomato sauce))
+
+(rember a lat)
+;; => '(soy and tomato sauce)
+
+;; My definition
+(define (get-first l)
+  (cond
+   ((null? l) '())
+   (else (first l))))
+
+(define (firsts ls)
+  (cond
+  ((null? ls) '())
+  (else (cons (get-first (first ls)) (firsts (rest ls))))))
+
+(firsts '((apple peach pumpkin)
+          (plum pear cherry)
+          (grape raisin pea)
+          (bean carrot eggplant)))
+;; => '(apple plum grape bean)
+
+(define l '((a b) (c d) (e f)))
+
+(firsts l)
+;; => '(a c e)
+
+(firsts '())
+;; => '()
+
+(define l '((five plums) (four) (eleven green oranges)))
+(firsts l)
+;; => '(five four eleven)
+
+(define l '(((five plums) four)
+            (eleven green oranges)
+            ((no) more)))
+
+(firsts l)
+;; => '((five plums) eleven (no))
+
+;; My definition
+
+(define (get-second l)
+  (first (rest l)))
+
+(define (seconds ls)
+  (cond
+   ((null? ls) '())
+   (else (cons (get-second (first ls)) (seconds (rest ls))))))
+
+(define l '((a b) (c d) (e f)))
+
+(seconds l)
+;; => '(b d f)
+
+;; There is an identification of the recursion patern detailed.
+;; (else (cons
+;; (car (car l)) ; typical element
+;; (firsts (cdr l)) ; natural recursion
+
+
